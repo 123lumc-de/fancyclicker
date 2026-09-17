5d736d8bb157f1f351dde59cf180035b2a9d6289
123lumc-de <123luminecraft@gmail.com>
2026-09-17 15:37:38 +0200

    Update DataManager.java

--- src/main/java/com/lmcstudios/cookieclicker/data/DataManager.java
+++ src/main/java/com/lmcstudios/cookieclicker/data/DataManager.java
@@ -15,7 +15,6 @@ public class DataManager {
     private final CookieClickerPlugin plugin;
     private final File file;
     private YamlConfiguration config;
-
     private final Map<UUID, PlayerData> cache = new HashMap<>();
 
     public DataManager(CookieClickerPlugin plugin) {
@@ -37,9 +36,7 @@ public class DataManager {
     }
 
     public void saveAll() {
-        for (PlayerData data : cache.values()) {
-            writeToConfig(data);
-        }
+        for (PlayerData data : cache.values()) writeToConfig(data);
         try {
             config.save(file);
         } catch (IOException e) {
@@ -52,30 +49,34 @@ public class DataManager {
         config.set(path + ".cookies", data.getCookies());
         config.set(path + ".total", data.getTotalCookies());
         config.set(path + ".level", data.getLevel());
+        config.set(path + ".preference", data.getPreference().name());
     }
 
     public PlayerData get(UUID uuid) {
-        if (cache.containsKey(uuid)) {
-            return cache.get(uuid);
-        }
+        if (cache.containsKey(uuid)) return cache.get(uuid);
         String path = uuid.toString();
         long cookies = config.getLong(path + ".cookies", 0);
         long total = config.getLong(path + ".total", 0);
         int level = config.getInt(path + ".level", 1);
-        PlayerData data = new PlayerData(uuid, cookies, total, level);
+        PlayerData.Preference pref;
+        try {
+            pref = PlayerData.Preference.valueOf(config.getString(path + ".preference", "LEFT"));
+        } catch (IllegalArgumentException e) {
+            pref = PlayerData.Preference.LEFT;
+        }
+        PlayerData data = new PlayerData(uuid, cookies, total, level, pref);
         cache.put(uuid, data);
         return data;
     }
 
-    public PlayerData get(OfflinePlayer player) {
-        return get(player.getUniqueId());
-    }
+    public PlayerData get(OfflinePlayer player) { return get(player.getUniqueId()); }
 
     public void reset(UUID uuid) {
         cache.remove(uuid);
         config.set(uuid.toString(), null);
-        PlayerData fresh = new PlayerData(uuid, 0, 0, 1);
+        PlayerData fresh = new PlayerData(uuid, 0, 0, 1, PlayerData.Preference.LEFT);
         cache.put(uuid, fresh);
+        saveAll();
     }
 
     public boolean hasData(UUID uuid) {
