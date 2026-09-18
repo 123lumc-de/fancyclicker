6095a53f67565e518964b98d5cad4a5550a8e6a2
123lumc-de <123luminecraft@gmail.com>
2026-09-18 16:03:17 +0200

    1

--- src/main/java/com/lmcstudios/fancyclicker/gui/ClickerGUI.java
+++ src/main/java/com/lmcstudios/fancyclicker/gui/ClickerGUI.java
@@ -59,7 +59,7 @@ public class ClickerGUI {
         long perClick = clickValue(data.getLevel());
         long nextCost = nextLevelCost(data.getLevel());
 
-    double rate = plugin.getConfig().getDouble("economy.money-per-cookie", 1.0);
+    double rate = plugin.getConfig().getDouble("economy.money-per-cookie", 0.1);
     double moneyValue = data.getCookies() * rate;
 
         inv.setItem(SLOT_INFO, buildItem(
