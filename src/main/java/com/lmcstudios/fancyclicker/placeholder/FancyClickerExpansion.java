4c360e0eb3cb56f2fe3086b7ecb99083dde2c924
123lumc <123luminecraft@gmail.com>
2026-09-23 16:01:12 +0200

    Update FancyClickerExpansion.java

--- src/main/java/com/lmcstudios/fancyclicker/placeholder/FancyClickerExpansion.java
+++ src/main/java/com/lmcstudios/fancyclicker/placeholder/FancyClickerExpansion.java
@@ -3,9 +3,12 @@ package com.lmcstudios.fancyclicker.placeholder;
 import com.lmcstudios.fancyclicker.FancyClickerPlugin;
 import com.lmcstudios.fancyclicker.data.PlayerData;
 import com.lmcstudios.fancyclicker.gui.ClickerGUI;
+import com.lmcstudios.fancyclicker.util.NumberUtil;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
 
 public class FancyClickerExpansion extends PlaceholderExpansion {
 
@@ -39,19 +42,20 @@ public class FancyClickerExpansion extends PlaceholderExpansion {
     public String onRequest(OfflinePlayer player, @NotNull String params) {
         if (player == null) return "";
         PlayerData data = plugin.getDataManager().get(player);
+
         double rate = plugin.getConfig().getDouble("economy.cookies-per-money", 10.0);
         double moneyExact = rate > 0 ? data.getCookies() / rate : 0;
-        long nextCost = (long) (plugin.getConfig().getLong("level-up.base-cost", 5000) * data.getLevel());
+        long nextCost = plugin.getConfig().getLong("level-up.base-cost", 5000) * data.getLevel();
 
         return switch (params.toLowerCase()) {
-            case "total" -> String.valueOf(data.getTotalCookies());
-            case "currently" -> String.valueOf(data.getCookies());
+            case "total" -> NumberUtil.format(data.getTotalCookies());
+            case "currently" -> NumberUtil.format(data.getCookies());
             case "level" -> String.valueOf(data.getLevel());
-            case "perclick" -> String.valueOf(ClickerGUI.clickValue(data.getLevel()));
-            case "money" -> String.format("%.2f", moneyExact);
-            case "money_rounded" -> String.valueOf(Math.round(moneyExact));
+            case "perclick" -> NumberUtil.format(ClickerGUI.clickValue(data.getLevel()));
+            case "money" -> String.format(Locale.US, "%.2f", moneyExact);
+            case "money_rounded" -> NumberUtil.format(Math.round(moneyExact));
             case "nextlevel" -> String.valueOf(data.getLevel() + 1);
-            case "nextcost" -> String.valueOf(nextCost);
+            case "nextcost" -> NumberUtil.format(nextCost);
             case "preference" -> data.getPreference().name();
             case "preference_friendly" -> data.getPreference() == PlayerData.Preference.LEFT
                     ? "Linksklick" : "Rechtsklick";
